43852acc6135076f6c7cdee1487cb9bcaada21d5
Hassan Afrouzi <hassan@afrouzi.com>
2023-08-25 13:18:18 -0400

    added signal_based solver method to DRIPs.m

--- src/Drip.m
+++ src/Drip.m
@@ -26,6 +26,7 @@
 %     * w         [= 1]        [updating weight on the new guess in iteration]
 %     * tol       [= 1e-4]     [tolerance level for convergence]
 %     * maxit     [= 10000]    [maximum number of iterations]
+%     * method    [= 'default'][method for solving the problem: 'spectral' or 'signal_based']
 %
 %% OUTPUTS
 %  The function returns a structure with the primitives and the solution objects:
@@ -52,6 +53,7 @@ function p = Drip(omega,beta,A,Q,H,varargin)
     addOptional(args,'fcap',false,@islogical);
     addOptional(args,'w',1);
     addOptional(args,'maxit',10000);
+    addOptional(args,'method','spectral')
 
     parse(args,varargin{:});
 
@@ -88,26 +90,55 @@ function p = Drip(omega,beta,A,Q,H,varargin)
             omega = (2^(2*kappa)/det(max(D,omega*I)))^(-1/n);
         end
         
-        % use the policy function in Afrouzi and Yang to get the implied
-        % posterior covariance:
-        p.ss.Sigma_p = omega*SqRSigma*U/(max(D,omega*I))*U'*SqRSigma;
-        
-        % get the new guess for prior covariance and calculate the
-        % convergence error:
-        p.ss.Sigma_1 = A*p.ss.Sigma_p*A' + Q*Q' ;
-        
-        % use the Euler equation in Afrouzi and Yang to update guess for
-        % Omega given update weight w:
-        SqRSigma    = real(sqrtm(Sigma0));
-        invSqRSigma = real(pinv(SqRSigma));
-        Omega0      = Omega_c + beta*A'*invSqRSigma*U*(min(D,omega*I))*U'*invSqRSigma*A;
-        
-        p.ss.err     = norm(p.ss.Sigma_1 - Sigma0)/norm(Sigma0) ...
-                     + norm(p.ss.Omega - Omega0)/norm(Omega0);
-        
+        if args.Results.method == "spectral"
+            % use the spectral method in Afrouzi and Yang to get the implied
+            % posterior covariance:
+            p.ss.Sigma_p = omega*SqRSigma*U/(max(D,omega*I))*U'*SqRSigma;
+            
+            % get the new guess for prior covariance and calculate the
+            % convergence error:
+            p.ss.Sigma_1 = A*p.ss.Sigma_p*A' + Q*Q' ;
+            
+            % use the Euler equation in Afrouzi and Yang to update guess for
+            % Omega given update weight w:
+            SqRSigma    = real(sqrtm(Sigma0));
+            invSqRSigma = real(pinv(SqRSigma));
+            Omega0      = Omega_c + beta*A'*invSqRSigma*U*(min(D,omega*I))*U'*invSqRSigma*A;
+        elseif args.Results.method == "signal_based"
+            % use the signal-based method in Afrouzi and Yang to get the implied
+            % vector of optimal signals:
+            d  = diag(D)     ;
+            
+            % find the indeces of eigenvalues that are larger than omega (number of optimal signals)
+            ii = find(d>omega) ;
+            % initial guess for the optimal signals
+            Y    = zeros(n,length(ii)) ;
+            temp1= zeros(n,n) ;
+            temp2= zeros(n,n) ;
+            
+            SqRSigma    = real(sqrtm(Sigma0));
+            invSqRSigma = real(pinv(SqRSigma));
+            for i = 1:length(ii)
+                Y(:,i) =invSqRSigma*U(:,ii(i)) ;
+                temp1 = temp1 + (d(ii(i))-omega)*Y(:,i)*Y(:,i)' ; 
+                temp2 = temp2 + (1-omega/d(ii(i)))*Y(:,i)*Y(:,i)' ; 
+            end
+
+            % get the implied posterior covariance and omega0
+            p.ss.Sigma_p = Sigma0 - Sigma0*(temp2)*Sigma0;
+            Omega0       = Omega_c - beta*A'*(temp1)*A + beta*A'*p.ss.Omega*A;
+
+            % get the new guess for prior covariance and calculate the
+            % convergence error:
+            p.ss.Sigma_1 = A*p.ss.Sigma_p*A' + Q*Q' ;
+        else 
+            error('method must be either "spectral" or "signal_based"')
+        end
         % update the guesses
+        p.ss.err     = norm(p.ss.Sigma_1 - Sigma0)/norm(Sigma0) ...
+                        + norm(p.ss.Omega - Omega0)/norm(Omega0);
         Sigma0      = args.Results.w*p.ss.Sigma_1 + (1-args.Results.w)*Sigma0 ;
-        p.ss.Omega   = args.Results.w*(Omega0) ...
+        p.ss.Omega  = args.Results.w*(Omega0) ...
                         +(1-args.Results.w)*p.ss.Omega;
         p.ss.Omega = (abs(p.ss.Omega)>1e-10).*p.ss.Omega ;
 
